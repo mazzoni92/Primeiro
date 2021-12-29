9ebb0f5a35ff6636b5556287e4a088e21bd73765
Leandro <77117871+mazzoni92@users.noreply.github.com>
2021-12-29 12:19:47 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello ";
+            var b = " Word";
+            Console.WriteLine(a + b);
+        }
+    }
+}
